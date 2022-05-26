0cee060c9efea1dd3475b4adb9245765fd5d9115
Lívia Morais <104389308+livmorais@users.noreply.github.com>
2022-05-26 19:54:43 -0300

    Add files via upload

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://livmorais:<password>@url-shortener-dio.hkinl.mongodb.net/?retryWrites=true&w=majority',
+}
