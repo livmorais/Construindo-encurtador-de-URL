1eaa1e05096b6198ae838de54fe9d2cc1001ad46
Lívia Morais <104389308+livmorais@users.noreply.github.com>
2022-05-26 19:54:07 -0300

    Add files via upload

---
+++ dist/config/Constants.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    API_URL: 'http://localhost:5000',
+    MONGO_CONNECTION: 'mongodb+srv://livmorais:<password>@url-shortener-dio.hkinl.mongodb.net/?retryWrites=true&w=majority',
+};
+//# sourceMappingURL=Constants.js.map
